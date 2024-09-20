f2062bc021e07a7a153fe0c1b89bd086fefaac37
Леталин <letalin06@bk.ru>
2024-09-21 02:27:50 +0300

    update code

--- main.go
+++ main.go
@@ -5,5 +5,11 @@ import (
 )
 
 func main() {
-	fmt.Println("Привет гит")
+	nums := []int{1, 1, 1, 2, 2, 6, 5, 6}
+	occurrences := make(map[int]int)
+
+	for _, num := range nums {
+		occurrences[num]++
+	}
+	fmt.Println(occurrences)
 }
